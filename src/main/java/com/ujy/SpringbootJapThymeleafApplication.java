231053b08c0a4b12b0cb760dae079e9b9c4a0ee3
study0909 <59643851+study0909@users.noreply.github.com>
2020-01-08 19:57:10 +0800

    Update SpringbootJapThymeleafApplication.java

--- src/main/java/com/ujy/SpringbootJapThymeleafApplication.java
+++ src/main/java/com/ujy/SpringbootJapThymeleafApplication.java
@@ -2,7 +2,7 @@ package com.ujy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//注释****************
 @SpringBootApplication
 public class SpringbootJapThymeleafApplication {
     /**
